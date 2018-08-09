880a07ff9b462b1eec29a3dafdd0c8c847d82bad
Alex0xf <1570259049@qq.com>
2018-08-09 17:34:42 +0800

    quartz定时任务

--- src/com/javasm/timmer/MyJob.java
+++ src/com/javasm/timmer/MyJob.java
@@ -1,8 +1,30 @@
 package com.javasm.timmer;
 
-public class MyJob {
+import java.util.List;
 
-	public void doJob(){
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.javasm.book.model.Book;
+import com.javasm.book.service.IBookService;
+import com.javasm.util.HttpClientDemo;
+
+/**
+ * 
+ * ClassName: MyJob 
+ * @Description: 任务调度 执行的类
+ * @author Alex
+ * @date 2018年8月9日
+ */
+public class MyJob {
+	
+	@Autowired
+	IBookService bookService;
+	
+	public void doJob(){//定时任务 可以链接数据库
 		System.out.println("测试dojob");
+		List<Book> list=bookService.selectBookList();
+		System.out.println(list);
+		HttpClientDemo.testPost();
 	}
+	
 }
